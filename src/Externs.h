d56d39e845cf7bbf986b53f57e81407028c46470
Miocasa <narimanabdualiev06@gmail.com>
2026-09-05 23:49:34 +0500

    WidgetManager and SaveManager extern added.

--- src/Externs.h
+++ src/Externs.h
@@ -4,8 +4,12 @@
 
 #include "ExternalRTC.h"
 #include "PowerManager.h"
+#include "SaveManager/SaveManager.h"
+#include "Widgets/WidgetManager.h"
 
 extern DisplayManager display;
 extern ExternalRTC externalRTC;
 extern NRFPowerManager power;
+extern WidgetManager widgetManager;
+extern SaveManager saveManager;
 #endif //EINK_EXTERNS_H
