511a5757871144a2fd1b45b97b48445145962d07
Miocasa <narimanabdualiev06@gmail.com>
2026-09-13 17:59:37 +0500

    Renaming folder and new methods load config, last screen and etc.

--- src/Settings/SaveManager.h
+++ src/Settings/SaveManager.h
@@ -6,17 +6,18 @@
 #define EINK_SAVEMANAGER_H
 #include <memory>
 #include <vector>
-#include <InternalFileSystem.h>
-
+// #include <InternalFileSystem.h>
+#include <QSPIFileSystem.h>
 #include "Results.h"
+#include "Settings.h"
 #include "Widgets/BackgroundImage.h"
 #include "Widgets/FontManager.h"
 #include "Widgets/TextBox.h"
 #include "Widgets/Widget.h"
 #include "Widgets/widget_t.h"
 
-#define PATH_MAX_LENGTH 256
-#define WIDGET_FILE_FORMAT ("%s/widget_%d.bin")
+#define WIDGET_FILE_NAME_HEADER "widget"
+#define WIDGET_FILE_FORMAT ("%s/%s_%d.bin")
 #define BACKGROUND_FILE_FORMAT ("%s/background.bin")
 #define SAVEMANAGER_VERSION (1.0)
 
@@ -26,15 +27,39 @@ public:
 	SaveManager();
 	~SaveManager() = default;
 	bool begin(bool format_on_fail = false);
+
+	/**
+	 * @brief Loads config from config.bin file, calls in WidgetManager
+	 * @param widgets vector of widgets from WidgetManager
+	 * @param bg_img
+	 * @return RESULTS class enum
+	 */
+	RESULTS loadConfig(std::vector<std::shared_ptr<Widget>>* widgets, BackgroundImage* bg_img);
+
+	RESULTS saveConfig();
+
+	/**
+	 * @brief if loadConfig finish with error load default
+	 * @param widgets vector of widgets from WidgetManager
+	 * @param bg_img
+	 * @return RESULTS class enum
+	 */
+	void loadDefaultScreen(std::vector<std::shared_ptr<Widget>>* widgets, BackgroundImage* bg_img);
+
+	RESULTS saveScreen(const char* path, std::vector<std::shared_ptr<Widget>>* widgets, BackgroundImage* bg_img);
 	RESULTS saveWidgets(const char* path, std::vector<std::shared_ptr<Widget>>* widgets);
 	RESULTS loadWidgets(const char* path, std::vector<std::shared_ptr<Widget>>* widgets);
 
-	RESULTS saveBackground(const char* path, std::shared_ptr<BackgroundImage>* bg_img);
-	RESULTS loadBackground(const char* path, std::shared_ptr<BackgroundImage>* bg_img);
+	RESULTS saveBackground(const char* path, BackgroundImage* bg_img);
+	RESULTS loadBackground(const char* path, BackgroundImage* bg_img);
 
 private:
 	char _path_buffer[PATH_MAX_LENGTH];
 	size_t _size;
+
+	Adafruit_FlashTransport_QSPI _flashTransport;
+	Adafruit_SPIFlash _externalFlash;
+	QSPIFileSystem qspiFS;
 	Adafruit_LittleFS_Namespace::File file;
 
 	void create_path(const char* path, uint8_t num);
