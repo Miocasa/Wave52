42021187463daa370e7147e57850d9bc8dc437db
Miocasa <narimanabdualiev06@gmail.com>
2026-09-13 17:39:00 +0500

    Changing categories and additional params.

--- src/config.h
+++ src/config.h
@@ -1,17 +1,32 @@
 #pragma once
 
+/**
+ * @file config.h
+ * @brief General configs, most of them can be redefined
+ */
 
-#define Weact4_2_2bit
-// #define GxEPD2_420_GDEY042T81 // analog define for Weact4_2_2bit
-
+/* --- Macros --- */
 #define SEC_TO_MS(s) (s * 1000)
 
-// --- E-ink pins---
-// #define EPD_MOSI_PIN   D10
-// #define EPD_MISO_PIN   D9
-// #define EPD_SCK_PIN    D8
+/**
+ * @brief Display type definition, now support only WeAct 4.2 2bit color depth (Weact4_2_2bit)
+ */
+#define Weact4_2_2bit
 
-#define EPD_BUSY_PIN   D9 // TODO change pin it conflict with
+
+/* ---  Default values --- */
+#define WFI_TIMEOUT SEC_TO_MS(10) // not implemented yet
+#define SLEEP_TIMEOUT SEC_TO_MS(30)
+
+// --- Constants ---
+#define DEVICE_NAME "Wave52" // BLE name
+#define CONFIG_PATH "/config.bin"
+
+/* --- SPI/E-ink pins--- */
+#define EPD_MOSI_PIN   D10
+#define EPD_MISO_PIN   D9
+#define EPD_SCK_PIN    D8
+#define EPD_BUSY_PIN   D9 // TODO change pin it conflict with EPD_MISO_PIN
 #define EPD_RST_PIN    D7
 #define EPD_DC_PIN     PIN_NFC2
 #define EPD_CS_PIN     PIN_NFC1
@@ -20,10 +35,8 @@
 // #define SDA D4
 // #define SCL D5
 
-
-// --- Wake interuptions ---
+// --- Wake interruption pins ---
 #define WAKE_BUTTON_PIN   D0
 #define WAKE_DS3231_PIN   D1
 
-// --- Sleep ---
-#define SLEEP_DELAY_MS SEC_TO_MS(30)
+
